52d99a76a51c7fe62502f15afdff57b9e987ca01
vinnyvilasboa <vinnycesca@gmail.com>
2022-04-02 11:51:36 -0400

    minor updates

---  main.js
+++  main.js
@@ -1,6 +1,6 @@
 const api = {
     key: "772a5938a64f5d0f8aa3e42401341db0",
-    baseurl: "https://api.openweathermap.org/data/2.5/"
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box'); //get
@@ -8,7 +8,18 @@ searchbox.addEventListener('keypress', setQuery); //pass keypress
 
 function setQuery(evt){
     if(evt.keyCode == 13) {
-        // getResults(searchBox.value);
+        getResults(searchbox.value);
         console.log(searchbox.value)
     }
+}
+
+function getResults (query){
+    fetch(`{api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json().data;
+    }).then(displayResults)
+}
+
+function displayResults (weather){
+    console.log(weather)
 }
